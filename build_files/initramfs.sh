27daa6acf78aaae89fdab2967e4b0f9606deead8
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-07-12 13:34:40 +0200

    Update initramfs.sh

--- build_files/initramfs.sh
+++ build_files/initramfs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -ouex pipefail
+
 KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
