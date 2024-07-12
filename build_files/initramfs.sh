4dcbede1d188fb8702e2b989e799831d803e33b0
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-07-12 12:56:16 +0200

    Update initramfs.sh

--- build_files/initramfs.sh
+++ build_files/initramfs.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/bash
 
-set -ouex pipefail
-
-# Remove nvidia specific files
-if [[ "${IMAGE_FLAVOR}" =~ "nvidia" ]]; then
-  :
-else
-  rm /usr/lib/dracut/dracut.conf.d/95-nvidia.conf
-  rm /usr/lib/modprobe.d/nvidia.conf
-fi
-
 KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
