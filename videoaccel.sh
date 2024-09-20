c1c7adef3e34f3005cf7847d2fa389a87d5d9aa2
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-09-20 17:47:52 +0200

    Update videoaccel.sh

--- videoaccel.sh
+++ videoaccel.sh
@@ -2,18 +2,18 @@
 
 # New file dedicated just for media acceleration, because the amount of gstreamer packages I was adding became excessive
 rpm-ostree install \
-  svt-av1 \
-  svt-vp9 \
-  gstreamer1-vaapi \
-  gstreamer1-plugin-openh264 \
-  gstreamer1-plugins-bad-free-opencv \
-  gstreamer1-plugins-bad-free-libs \
-  gstreamer1-plugins-bad-free-extras \
-  gstreamer1-plugin-gtk4 \
-  gstreamer1-plugins-ugly-free \
-  gstreamer1-plugins-bad-freeworld \
-  gstreamer-plugins-espeak \
-  gstreamer-ffmpeg \
-  gstreamer1-libav \
-  gstreamer-plugins-ugly \
-  gstreamer-plugins-bad
+svt-av1 \
+svt-vp9 \
+gstreamer1-vaapi \
+gstreamer1-plugin-openh264 \
+gstreamer1-plugins-bad-free-opencv \
+gstreamer1-plugins-bad-free-libs \
+gstreamer1-plugins-bad-free-extras \
+gstreamer1-plugin-gtk4 \
+gstreamer1-plugins-ugly-free \
+gstreamer1-plugins-bad-freeworld \
+gstreamer-plugins-espeak \
+gstreamer-ffmpeg \
+gstreamer1-libav \
+gstreamer-plugins-ugly \
+gstreamer-plugins-bad
