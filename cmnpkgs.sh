d0599f8132e69377942a0fe8564139c2aad2e4ca
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-09-20 19:45:03 +0200

    Create cmnpkgs.sh

---
+++ cmnpkgs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+### Separate file for this part of the build process. The goal is to unify parts of the build process by making modules that are used across several builds
+rpm-ostree install \
+gnome-randr-rust \
+polkit \
+neofetch \
+fastfetch \
+grub-customizer \
+gnome-tweaks \
+distrobox \
+yt-dlp \
+zlib \
+xcb-util-cursor \
+i2c-tools
