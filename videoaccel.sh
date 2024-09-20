9539054c9ff32f27e68c946dc289d908a4280005
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-09-20 16:20:47 +0200

    Update videoaccel.sh

--- videoaccel.sh
+++ videoaccel.sh
@@ -3,3 +3,21 @@
 # New file dedicated just for media acceleration
 rpmostree install \
   gstreamer \
+  gstreamer-plugins-base \
+  gstreamer-plugins-ugly \
+  gstreamer-plugins-espeak \
+  gstreamer-ffmpeg \
+  svt-av1 \
+  svt-av1-libs \
+  svt-vp9 \
+  svt-vp9-libs \
+  gstreamer1-vaapi \
+  gstreamer1-plugin-openh264 \
+  gstreamer1-plugins-bad-free-opencv \
+  gstreamer1-plugins-bad-free-libs \
+  gstreamer1-plugins-bad-free-extras \
+  gstreamer1-plugins-bad-nonfree \
+  gstreamer1-plugin-gtk4 \
+  gstreamer1-plugins-ugly-free \
+  gstreamer1-plugins-bad-freeworld \
+  gstreamer1-libav && \
