33e4aaaf0f2813e2e276d9fa5a253c9b43d7f301
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-09-26 19:37:09 +0200

    update cmnpkgs

--- scripts/cmnpkgs.sh
+++ scripts/cmnpkgs.sh
@@ -15,3 +15,29 @@ yt-dlp \
 zlib \
 xcb-util-cursor \
 i2c-tools
+
+# Pop in Valve's patched stuff
+rpm-ostree override replace --experimental\
+--from repo=copr:copr.fedorainfracloud.org:kylegospo:bazzite-multilib \
+mesa-filesystem \
+mesa-libxatracker \
+mesa-libglapi \
+mesa-dri-drivers \
+mesa-libgbm \
+mesa-libEGL \
+mesa-vulkan-drivers \
+mesa-libGL \
+pipewire \
+pipewire-alsa \
+pipewire-gstreamer \
+pipewire-jack-audio-connection-kit \
+pipewire-jack-audio-connection-kit-libs \
+pipewire-libs \
+pipewire-pulseaudio \
+pipewire-utils \
+pipewire-plugin-libcamera \
+bluez \
+bluez-obexd \
+bluez-cups \
+bluez-libs \
+xorg-x11-server-Xwayland
