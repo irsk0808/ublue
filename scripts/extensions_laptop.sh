8af3e935ce30bf428eedbc75feb15395c4d123bd
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-09-29 10:19:16 +0200

    add laptop specific extensions

---
+++ scripts/extensions_laptop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Laptop specific extensions
+# Hide Top Bar
+wget -P /tmp https://extensions.gnome.org/extension-data/hidetopbarmathieu.bidon.ca.v117.shell-extension.zip
+unzip -d /tmp/tmpdir /tmp/hidetopbarmathieu.bidon.ca.v117.shell-extension.zip
+UUID=$(jq -r '.["uuid"]' < '/tmp/tmpdir/metadata.json')
+cp -r tmp/tmpdir /usr/share/gnome-shell/extensions/${UUID} && chmod 755 /usr/share/gnome-shell/extensions/${UUID}/metadata.json
+glib-compile-schemas /usr/share/gnome-shell/extensions/${UUID}/schemas/
+rm -r /tmp/tmpdir
+
+# Wallpaper Slideshow
+wget -P /tmp https://extensions.gnome.org/extension-data/azwallpaperazwallpaper.gitlab.com.v10.shell-extension.zip
+unzip -d /tmp/tmpdir /tmp/azwallpaperazwallpaper.gitlab.com.v10.shell-extension.zip
+UUID=$(jq -r '.["uuid"]' < '/tmp/tmpdir/metadata.json')
+cp -r tmp/tmpdir /usr/share/gnome-shell/extensions/${UUID} && chmod 755 /usr/share/gnome-shell/extensions/${UUID}/metadata.json
+glib-compile-schemas /usr/share/gnome-shell/extensions/${UUID}/schemas/
+rm -r /tmp/tmpdir
