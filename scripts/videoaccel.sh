3396d14c9782d76de1aa2e4652f353b4ffccc8b9
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-10-06 16:23:40 +0200

    Revert "Update videoaccel.sh"
    This reverts commit af371075a76e2c2eea7f4a4e90ee4f655beac8dc.

--- scripts/videoaccel.sh
+++ scripts/videoaccel.sh
@@ -5,13 +5,13 @@ rpm-ostree install \
 svt-av1 \
 svt-vp9 \
 gstreamer1-vaapi \
+gstreamer1-plugins-bad-free-opencv \
 gstreamer1-plugins-bad-free-extras \
 gstreamer1-plugin-gtk4 \
 gstreamer1-plugins-ugly \
 gstreamer1-plugins-bad-freeworld \
 gstreamer1-libav \
 gstreamer-plugins-espeak
-# gstreamer1-plugins-bad-free-opencv \
 # gstreamer1-plugin-openh264 \
 # gstreamer1-plugins-bad-free-libs \
 # gstreamer1-plugins-ugly-free \
