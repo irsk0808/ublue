09dcc183b20c0c93ee0138aa708ae859ea194618
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-10-08 22:53:46 +0200

    Update videoaccel.sh

--- scripts/videoaccel.sh
+++ scripts/videoaccel.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # New file dedicated just for media acceleration, because the amount of gstreamer packages I was adding became excessive
+rpm-ostree install \
+svt-av1 \
+svt-vp9 \
+gstreamer1-plugin-openh264 \
+gstreamer1-vaapi \
+gstreamer-plugins-espeak
 #rpm-ostree override remove \
 #gstreamer1-plugins-bad-free-libs \
 #gstreamer1-plugins-ugly-free && \
