57794114dfa3ee95eccda01512b7a36283d5f34a
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-10-08 22:09:57 +0200

    Update videoaccel.sh

--- scripts/videoaccel.sh
+++ scripts/videoaccel.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # New file dedicated just for media acceleration, because the amount of gstreamer packages I was adding became excessive
-rpm-ostree override remove \
-gstreamer1-plugins-bad-free-libs \
-gstreamer1-plugins-ugly-free && \
-rpm-ostree install \
-svt-av1 \
-svt-vp9 \
-gstreamer1-vaapi \
-gstreamer1-plugin-openh264 \
-gstreamer1-plugins-bad-free-opencv \
-gstreamer1-plugins-bad-free-extras \
-gstreamer1-plugin-gtk4 \
-gstreamer1-plugins-ugly \
-gstreamer1-plugins-bad-freeworld \
-gstreamer1-libav \
-gstreamer-plugins-espeak
+#rpm-ostree override remove \
+#gstreamer1-plugins-bad-free-libs \
+#gstreamer1-plugins-ugly-free && \
+#rpm-ostree install \
+#svt-av1 \
+#svt-vp9 \
+#gstreamer1-vaapi \
+#gstreamer1-plugin-openh264 \
+#gstreamer1-plugins-bad-free-opencv \
+#gstreamer1-plugins-bad-free-extras \
+#gstreamer1-plugin-gtk4 \
+#gstreamer1-plugins-ugly \
+#gstreamer1-plugins-bad-freeworld \
+#gstreamer1-libav \
+#gstreamer-plugins-espeak
 #gstreamer-plugins-espeak \
 #gstreamer-ffmpeg \
 #gstreamer-plugins-ugly \
