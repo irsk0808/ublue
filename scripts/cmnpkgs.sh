9b021aef820e7c815ee49c3b527bc11278e9b12b
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-12-03 20:13:50 +0100

    Update cmnpkgs.sh

--- scripts/cmnpkgs.sh
+++ scripts/cmnpkgs.sh
@@ -37,3 +37,9 @@ bluez-obexd \
 bluez-cups \
 bluez-libs \
 xorg-x11-server-Xwayland
+
+# Installing FirefoxPWA to save headaches later
+rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
+echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
+rpm-ostree --enablerepo="firefoxpwa"
+rpm-ostree install firefoxpwa
