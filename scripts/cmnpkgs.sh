b2224ff8af01c24c4b57eb524028c39b294b5f7e
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-12-03 20:27:05 +0100

    Revert "Update cmnpkgs.sh"
    This reverts commit b70e937d61ac510e6692758025e60eb8183cba86.

--- scripts/cmnpkgs.sh
+++ scripts/cmnpkgs.sh
@@ -23,6 +23,15 @@ google-noto-fonts-all
 # Pop in Bazzite patched stuff
 rpm-ostree override replace --experimental \
 --from repo=copr:copr.fedorainfracloud.org:kylegospo:bazzite-multilib \
+pipewire \
+pipewire-alsa \
+pipewire-gstreamer \
+pipewire-jack-audio-connection-kit \
+pipewire-jack-audio-connection-kit-libs \
+pipewire-libs \
+pipewire-pulseaudio \
+pipewire-utils \
+pipewire-plugin-libcamera \
 bluez \
 bluez-obexd \
 bluez-cups \
@@ -34,13 +43,3 @@ rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
 echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | sudo tee /etc/yum.repos.d/firefoxpwa.repo
 rpm-ostree --enablerepo="firefoxpwa"
 rpm-ostree install firefoxpwa
-
-#pipewire \
-#pipewire-alsa \
-#pipewire-gstreamer \
-#pipewire-jack-audio-connection-kit \
-#pipewire-jack-audio-connection-kit-libs \
-#pipewire-libs \
-#pipewire-pulseaudio \
-#pipewire-utils \
-#pipewire-plugin-libcamera \
