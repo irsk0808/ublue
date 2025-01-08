7082af28e77c6f51c3cc7dd3d4a226d686561d76
clc1101 <151733110+clc1101@users.noreply.github.com>
2025-01-08 22:33:48 +0100

    patch mutter

--- scripts/cmnpkgs.sh
+++ scripts/cmnpkgs.sh
@@ -21,11 +21,10 @@ google-noto-fonts-all \
 rsms-inter-fonts \
 xhost
 
-# Replace mutter with package from ublue's copr
+# Replace mutter with a patched one
 rpm-ostree override replace --experimental \
---from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-mutter \
-mutter-common
+--from repo=copr:copr.fedorainfracloud.org:gloriouseggroll:nobara-41 \
+mutter
 
 # Pop in Bazzite patched stuff
 rpm-ostree override replace --experimental \
