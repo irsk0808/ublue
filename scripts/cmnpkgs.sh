479c56e4985849f1845c3f691c238bfe41637e0d
clc1101 <151733110+clc1101@users.noreply.github.com>
2025-02-10 10:09:34 +0100

    Update cmnpkgs.sh

--- scripts/cmnpkgs.sh
+++ scripts/cmnpkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### Separate file for this part of the build process. The goal is to unify parts of the build process by making modules that are used across several builds
-rpm-ostree install \
+dnf5 -y install \
 gnome-randr-rust \
 gnome-monitor-config \
 fastfetch \
@@ -48,5 +48,5 @@ xorg-x11-server-Xwayland
 # Installing FirefoxPWA to save headaches later
 rpm --import https://packagecloud.io/filips/FirefoxPWA/gpgkey
 echo -e "[firefoxpwa]\nname=FirefoxPWA\nmetadata_expire=300\nbaseurl=https://packagecloud.io/filips/FirefoxPWA/rpm_any/rpm_any/\$basearch\ngpgkey=https://packagecloud.io/filips/FirefoxPWA/gpgkey\nrepo_gpgcheck=1\ngpgcheck=0\nenabled=1" | tee /etc/yum.repos.d/firefoxpwa.repo
-rpm-ostree --enablerepo="firefoxpwa"
-rpm-ostree install firefoxpwa
+dnf5 --enable-repo="firefoxpwa"
+dnf5 -y install firefoxpwa
