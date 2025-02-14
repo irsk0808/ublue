3d5671b55739a108c76abe7304618029dfb59596
irsk0808 <151733110+irsk0808@users.noreply.github.com>
2025-02-14 09:38:09 +0100

    modularize akmods

---
+++ scripts/akmods.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+echo "Installing Bazzite Kernel!"
+
+dnf5 -y remove --no-autoremove \
+    kernel \
+    kernel-core \
+    kernel-modules \
+    kernel-modules-core \
+    kernel-modules-extra
+
+dnf5 -y install \
+    /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+    /tmp/kernel-rpms/kernel-core-*.rpm \
+    /tmp/kernel-rpms/kernel-modules-*.rpm \
+    /tmp/kernel-rpms/kernel-uki-virt-*.rpm \
+    /tmp/kernel-rpms/kernel-devel-*.rpm
+    
+echo "Installing kmod packages!"
+
+dnf5 -y install /tmp/rpms/kmods/kmod-xpadneo*.rpm
+dnf5 -y install /tmp/rpms/kmods/kmod-xone*.rpm
