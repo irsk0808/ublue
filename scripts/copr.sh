bf13a3ddc0c8d6a12de8eb48f9bcb13601a877c5
irsk0808 <151733110+irsk0808@users.noreply.github.com>
2025-02-19 10:39:35 +0100

    what

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Modularizing the Copr instructions here too to make my life easier
-dnf5 -y copr enable fiftydinar/gnome-randr-rust && \
-dnf5 -y copr enable ublue-os/staging && \
-dnf5 -y copr enable kylegospo/bazzite-multilib && \
-dnf5 -y copr enable trixieua/mutter-patched && \
+dnf5 -y copr enable fiftydinar/gnome-randr-rust
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable kylegospo/bazzite-multilib
+dnf5 -y copr enable trixieua/mutter-patched
