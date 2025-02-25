dcd4ff12d7134796d0621fe0833669dd7b9aae69
irsk0808 <151733110+irsk0808@users.noreply.github.com>
2025-02-25 16:19:03 +0100

    Update copr.sh

--- scripts/copr.sh
+++ scripts/copr.sh
@@ -4,6 +4,7 @@ set -eoux pipefail
 
 # Modularizing the Copr instructions here too to make my life easier
 dnf5 -y copr enable fiftydinar/gnome-randr-rust
+dnf5 -y copr enable ublue-os/packages
 dnf5 -y copr enable ublue-os/staging
 dnf5 -y copr enable kylegospo/bazzite-multilib
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
